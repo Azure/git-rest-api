4700044d744eb2c0b57ce98739fa5861c45de850
Timothee Guerin <tiguerin@microsoft.com>
2019-05-15 12:18:40 -0700

    Wip

--- src/controllers/app.controller.ts
+++ src/controllers/app.controller.ts
@@ -3,7 +3,9 @@ import { AppService } from "../services";
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {
+    this.appService.getHello();
+  }
 
   @Get()
   getHello(): string {
