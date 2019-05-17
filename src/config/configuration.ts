d90c0b8dafaee03df2f8d59bd645ca8be2388da2
billytrend <witrend@microsoft.com>
2019-05-17 15:27:24 -0700

    Add node convict (#6)
    * Add node convict
    
    * Add test Env
    
    * Move property

---
+++ src/config/configuration.ts
@@ -0,0 +1,31 @@
+import { Injectable } from "@nestjs/common";
+
+import { developmentConfig } from "./development";
+import { productionConfig } from "./production";
+import { configSchema } from "./schema";
+import { testConfig } from "./test";
+
+export type Env = "production" | "development" | "test";
+
+@Injectable()
+export class Configuration {
+  public readonly env: Env;
+
+  constructor() {
+    const environmentOverrides: Record<Env, Partial<Configuration>> = {
+      production: developmentConfig,
+      development: productionConfig,
+      test: testConfig,
+    };
+
+    const env: Env = configSchema.get("env");
+
+    // Load environment dependent configuration
+    configSchema.load(environmentOverrides[env]);
+
+    // Perform validation
+    configSchema.validate({ allowed: "strict" });
+
+    this.env = configSchema.get("env");
+  }
+}
