9af0b3736d6d79ac5509f2605434a1156a5163c5
Billy Trend <witrend@microsoft.com>
2019-05-23 13:44:55 -0700

    Fix swagger

--- src/dtos/git-dir-object-content.ts
+++ src/dtos/git-dir-object-content.ts
@@ -1,11 +1,6 @@
-import { ApiModelProperty } from "@nestjs/swagger";
-
 import { GitObjectContent } from "./git-object-content";
 
 export class GitDirObjectContent extends GitObjectContent {
-  @ApiModelProperty({ type: "dir" })
-  public type: "dir" = "dir";
-
   constructor(gitObjectContent: GitDirObjectContent) {
     super(gitObjectContent);
   }
