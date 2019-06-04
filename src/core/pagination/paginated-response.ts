2d02908b38b8455a55579a42217f20efaa093853
Timothee Guerin <tiguerin@microsoft.com>
2019-06-04 08:38:16 -0700

    PRev

--- src/core/pagination/paginated-response.ts
+++ src/core/pagination/paginated-response.ts
@@ -9,9 +9,14 @@ export function applyPaginatedResponse(attributes: PaginatedList<any>, response:
   if (response.req) {
     const originalUrl = `${response.req.protocol}://${response.req.get("host")}${response.req.originalUrl}`;
     const nextUrl = new URL(originalUrl);
-
     nextUrl.searchParams.set("page", (attributes.page + 1).toString());
     const links = [`<${nextUrl}>; rel="next"`];
+
+    if (attributes.page > 1) {
+      const prevUrl = new URL(originalUrl);
+      prevUrl.searchParams.set("page", (attributes.page - 1).toString());
+      links.unshift(`<${prevUrl}>; rel="prev"`);
+    }
     response.setHeader("Link", links.join(", "));
   }
   response.send(attributes.items);
