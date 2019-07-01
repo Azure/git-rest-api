1e8bb217e386ebf384411cac23f66263f57dc84d
Timothee Guerin <tiguerin@microsoft.com>
2019-07-01 11:15:19 -0700

    Fix emiitter

--- src/core/logger/winston-logger.ts
+++ src/core/logger/winston-logger.ts
@@ -59,13 +59,12 @@ if (config.nodeEnv === "development") {
   );
 }
 
+const transports: any[] = [new winston.transports.Console(consoleTransport)];
+
 if (config.nodeEnv === "test") {
   consoleTransport.silent = true;
-}
-
-export const WINSTON_LOGGER_OPTIONS: LoggerOptions = {
-  transports: [
-    new winston.transports.Console(consoleTransport),
+} else {
+  transports.push(
     new winstonDailyFile({
       filename: `%DATE%.log`,
       datePattern: "YYYY-MM-DD-HH",
@@ -73,7 +72,11 @@ export const WINSTON_LOGGER_OPTIONS: LoggerOptions = {
       dirname: "logs",
       handleExceptions: true,
     }),
-  ],
+  );
+}
+
+export const WINSTON_LOGGER_OPTIONS: LoggerOptions = {
+  transports,
 };
 
 /**
